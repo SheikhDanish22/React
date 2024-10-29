037e7ba436f0f227e8a7c1544e2cb8ab1328a5b7
SheikhDanish22 <danish1521@gmail.com>
2024-10-29 09:44:53 +0530

    Recat

---
+++ src/App.jsx
@@ -0,0 +1,144 @@
+// // const App=()=>{
+// //   return(
+// //     <h1>WElcome</h1>
+// //   )
+// // }
+// // export default App;
+// // import React from "react";
+// // const name="Danish"
+// // const age=27;
+// // const App=()=>{
+// //   return(
+// //     <h1>Welcome To Bhopal {name} you are boy</h1>
+// //   )
+// // }
+// // export default App;
+
+
+// // const sub=<ol>
+// //          <li>Python</li>
+// //          <li>C++</li>
+// //          <li>Html</li>
+// //          <li>Js</li>
+// // </ol>;
+// // const App=()=>{
+// //   return(
+// //     <h1>our computer subjects :{sub}</h1>
+// //   )
+// // }
+// // export default App;
+
+
+
+// // const App=()=>{
+// //     return(
+// //       <> fragment tag
+// //        <h1>our computer subjects</h1>
+// //        <h2>Desktop</h2>
+// //        </>
+// //      )
+// //    }
+// //    export default App;
+  
+// //22-10-24
+
+// // import College from "./cybrom";
+// //  const App=()=>{
+// //   return(
+// //     <>
+// //        <h1>Application Form</h1>
+// //        Name : <input type="text" />
+// //        <br />
+// //        City : <input type="text" />
+// //        <hr  size="4" color="red"/>
+// //        <button>save</button>
+// //     </>
+// //   )
+// //  }
+// // export default App;
+
+// // 24/10
+
+// import Header from "./Header";
+// import Data from "./Data";
+// import Footer from "./Footer";
+// import TopMenu from "./TopMenu";
+// import BottomMenu from "./BottomMenu";
+// import TopData from "./TopData";
+// import MiddleData from "./MiddleData";
+// import EndData from "./EndData";
+// import Footer from "./Footer";
+// const App=()=>{
+//   return(
+//     <>
+//           <Header/>
+//           <TopMenu/>
+//           <BottomMenu/>
+//           <Data/>
+//           <TopData/>
+//           <MiddleData/>
+//           <EndData/>
+          
+//           <Footer/>
+          
+
+//          </>
+//   )
+// }
+// export default App;
+
+// 25/10/2024
+// import Collage from "./Collage"
+// const App=()=>{
+//   return(
+//     <>
+//     <h1>welcome</h1>
+//     <Collage nm="Danish" ct="Bhopal"/>
+    
+//     </>
+//   )
+// }
+// export default App;
+
+// import Collage from "./Collage";
+// const App=()=>{
+//   return(
+//     <>
+    
+//     <Collage name="cybrom" city="bhopal" clss="4" />
+//     </>
+//   )
+// }
+// export default App;
+
+
+
+// import Collage from "./Collage";
+//  const App=()=>{
+//    return(
+//      <>
+
+//     <h1>Welcome</h1>
+//     <Collage name="Danish" ct="Bhopal"/>
+//     <h2>Happy Diwali</h2>
+//      </>
+//    )
+//  }
+//  export default App;
+
+
+
+// import Collage from "./Collage";
+//   const App=()=>{
+//     return(
+//       <>
+//       <h1>Aman</h1>
+//       <Collage>Hiii</Collage>
+      
+//       </>
+//     )
+//   }
+//   export default App;
+
+
+
